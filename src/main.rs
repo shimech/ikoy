f2dc656e1e6c10177cc4d77283f60a8637077027
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2025-12-29 20:10:51 +0900

    Add v8 dependency and implement basic JavaScript execution in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let platform = v8::new_default_platform(0, false).make_shared();
+    v8::V8::initialize_platform(platform);
+    v8::V8::initialize();
+
+    let isolate = &mut v8::Isolate::new(Default::default());
+
+    let scope = std::pin::pin!(v8::HandleScope::new(isolate));
+    let scope = &mut scope.init();
+    let context = v8::Context::new(scope, Default::default());
+    let scope = &mut v8::ContextScope::new(scope, context);
+
+    let code = v8::String::new(scope, "'Hello' + ' World!'").unwrap();
+    println!("javascript code: {}", code.to_rust_string_lossy(scope));
+
+    let script = v8::Script::compile(scope, code, None).unwrap();
+    let result = script.run(scope).unwrap();
+    let result = result.to_string(scope).unwrap();
+    println!("result: {}", result.to_rust_string_lossy(scope));
 }
